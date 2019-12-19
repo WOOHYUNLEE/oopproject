12f235dc74b6db7bad89c0ed7f82266e7c60d2b0
Seulin <ths06123@gmail.com>
2019-12-20 05:31:30 +0900

    많이 바꿨어.. signup, login 적당히 짰다

--- AS/AS/AS.cpp
+++ AS/AS/AS.cpp
@@ -1,27 +1,132 @@
 ﻿
 
 #include "as.h"
+#include <cassert>
 
 
+Admin& Admin::getInst() {
+	static Admin instance;
+	return instance;
+}
+
+void Admin::helper(int& action, string& info, int& position) {
+	int id =0;
+	string name="ㅎㅎ";
+	list<string> subjects;
+	subjects.push_back("d");
+	switch (action) {
+	case 1:
+		//info를 id, name, subjects로 나눔
+		signup(position, id, name, subjects);
+		break;
+	case 2:
+		//info를 id, name으로 나눔
+		login(position, id, name);
+		break;
+	}
+}
+
+void Admin::signup(int& position, int& id, string& name, list<string>& subjects) {
+	switch (position) { //외부 데이터에 저장
+	case 1: {
+		Professor* prof = new Professor(id, name, subjects);
+		professors.insert(pair<int, Professor*>(id, prof));
+		break;}
+	case 2: {
+		Student* stud = new Student(id, name, subjects);
+		students.insert(pair<int, Student*>(id, stud));
+		break; }
+	}
+}
+
+void Admin::login(int position, int id, string name) {
+	try {
+		switch (position) {
+		case 1:
+			connector = professors.at(id);
+			break;
+		case 2:
+			connector = students.at(id);
+			break;
+		}
+	}
+	catch(...) {
+		cout << "Error, Please chek your information";
+		return;
+	}
+}
+
+string Admin::getToday() const {
+
+	return "string";
+}
+
+void Admin::assign(Assignment*) {
+}
+
+int Admin::getMaxNum_period(int date) const {
+	return 0;
+}
+
+int Admin::getMaxNum_day(int date) const {
+	return 0;
+}
+
+int Admin::getNum_ass(int date) const {
+	return 0;
+}
+
+void Admin::remove_user() {
+}
+
+void Admin::remove_assignment() {
+}
+
+bool Admin::operator==(const Admin& other) {
+	return this == &other;
+}
+
+void exit(int input) { if (input == 0) exit(-1); }
 
 int main() {
-	int value{ 0 };
-	string a{ "Shin" };
-	cout << "signup or login?\n";
-	cout << "1. signup \n2. login\n0. exit\n";
-	cin >> value;
-	cout << "Professor or Student?\n";
-	cout << "1. Professor \n2. Student\n0. exit\n";
-	cin >> value;
-	cout << "Write your information(id / name)\n";
-	cin >> value;
-	cout << "Professor " << a << ", Welcome to the Scheduler!\n";
-	cout << "What do you want to do?\n";
-	cout << "1. Add assignment \n2. Post office hour \n3. Check Students' assignment \n0. exit\n";
+	Admin& admin = Admin::getInst();
+	Admin& kk = Admin::getInst();
+	cout << (admin == kk) << endl; //admin은 1개!
+
+	do{ //input validation 없음
+		int action;
+		int position;
+		string info;
+		cout << "sign up or login?\n";
+		cout << "1. sign up \n2. login\n0. exit\n";
+		cin >> action;
+		exit(action);
+		cout << "professor or student?\n";
+		cout << "1. professor \n2. student\n0. exit\n";
+		cin >> position;
+		exit(position);
+		switch (action) {
+		case 1:
+			cout << "write your information(id / name / subject)\n";
+			cin.ignore();
+			getline(cin, info);
+			break;
+		case 2:
+			cout << "write your information(id / name)\n";
+			cin.ignore();
+			getline(cin, info);
+			break;
+		}
+		admin.helper(action, info, position);
+	} while (!admin.isconnected()); //가입 혹은 로그인하는 구간
+
+	cout << "professor " << 23 << ", welcome to the scheduler!\n";
+	cout << "what do you want to do?\n";
+	cout << "1. add assignment \n2. post office hour \n3. check students' assignment \n0. exit\n";
 	cout << "1\n";
-	cout << "Please write information of assignment\n       Subject      :   Name    :    Contents    : Due date\n";
-	cout << " 객체지향프로그래밍 :Assignment2: Making IVector : 20191212\n";
-	cout << "Warning! Some of the students have too many assginments!";
+	cout << "please write information of assignment\n       subject      :   name    :    contents    : due date\n";
+	cout << " 객체지향프로그래밍 :assignment2: making ivector : 20191212\n";
+	cout << "warning! some of the students have too many assginments!";
 
 
 	/*   int value{ 0 };
--- AS/AS/as.h
+++ AS/AS/as.h
@@ -1,48 +1,57 @@
 #pragma once
 
 #include <iostream>
+#include <string>
 #include <list>
 #include <map>
 using namespace std;
 
 void texteditor();
 
+class Admin;
+class User;
+class Student;
+class Professor;
+class Subject;
+class Assignment;
+
 
 class Admin final{
 private:
-	Admin(); // singleton 외부에서 관리자 객체 추가 생성을 막기 위해
-	static Admin* instance; 
-	Admin(const Admin&); //singleton
-	void operator=(const Admin&); //singleton
-	~Admin();
-
+	Admin() {}; // singleton 외부에서 관리자 객체 추가 생성을 막기 위해
+	~Admin() {};
 
 	int today = 1; //과제 지우거나 날짜 계산할 때 필요한 오늘 날짜
 	// 1월 1일=1,  12월 31일= 365
 	int in_to_today(); //input을 날짜 데이터로 바꿔서 int(today)를 리턴하는 함수
 	void tomorrow(); //today 값을 1 증가시는(내일이 되는 함수)
 
+	User* connector = nullptr;
 	map<string, Subject*> subjects; // 과목 고유 코드 및 과목 객체
-	list<Student*> students;
-	list<Professor*> professors;
+	map<int, Professor*> professors; // id, 교수 객체
+	map<int, Student*> students; // id, 학생 객체
 
-	User login(); //validation based on text(1)
-	void signup();//객체를 생성하고 동적할당 하고 텍스트 저장(1)
-	//교수면 교수랑, 과목을 만들고 자기한테 있는 목록에 추가
-	//학생이면, 입력한 과목이 있는 확인하고 학생을 만들고 과목 객체 목록에 자신을 추가하고 자신이 갖고 있는 목록에 과목을 추가한다.
+	void signup(int& position, int& id, string& name, list<string>& subjects);//객체를 생성하고 동적할당 하고 텍스트 저장(1)
+//교수면 교수랑, 과목을 만들고 자기한테 있는 목록에 추가
+//학생이면, 입력한 과목이 있는 확인하고 학생을 만들고 과목 객체 목록에 자신을 추가하고 자신이 갖고 있는 목록에 과목을 추가한다.
+	void login(int position, int id, string name); //validation based on text(1)
 
 public:
-	static Admin& getInst(); // singleton 객체 한 개만 만드는 코드
+	void helper(int& action, string& info, int& position); //input을 id, name, subject로 변환하고 signup이나 login을 부름
+	bool isconnected() { return connector != nullptr; }
+	User& getConnector() { return *connector; } //지금 접속중인 객체 반환
 
-	string getToday(); //오늘 날짜를 리턴하는 함수
+	static Admin& getInst(); // singleton 객체 만들거나 반환하는 함수
+
+	string getToday() const; //오늘 날짜를 리턴하는 함수
 
 	void assign(Assignment*); //과제에 있는 과목코드로 과목을 찾고 그 안에 과제를 넣어줌
 
 	//assign 함수에서 구현하는 데에 필요한 3함수?
-	int getMaxNum_period(int date);
+	int getMaxNum_period(int date) const;
 	//수강하는 학생들 중에 date를 기준으로 며칠 이내에 과제 개수의 최댓값
-	int getMaxNum_day(int date); //학생들 중 같은 날에 마감인 다른 과제의 개수의 최댓값
-	int getNum_ass(int date); //date에 마감인 과제의 개수, getMaxNum 두 함수에서 사용해야 할 함수임.
+	int getMaxNum_day(int date) const; //학생들 중 같은 날에 마감인 다른 과제의 개수의 최댓값
+	int getNum_ass(int date) const; //date에 마감인 과제의 개수, getMaxNum 두 함수에서 사용해야 할 함수임.
 
 	void remove_user(); //User 객체를 지움(동적할당 해제, 텍스트(1)에서 삭제)
 	//교수를 지우면 과목도 지우고 학생들한테 있는 과목 코드에서도 삭제해야 함.
@@ -52,36 +61,48 @@ public:
 	//늦게 제출할 수도 있으니 마감일으로부터 3일이 지나면 지우기
 	//날짜가 바뀌면 자동으로 실행
 
+	bool operator==(const Admin& other); //must be removed
 };
 
 class User {
-	string name;
 	int id;
+	string name;
+	list<string> sub_id; //과목 고유 코드
+public:
+	User() {};
+	User(int& i_id, string& i_name, list<string>& i_sub_id) {
+		id = i_id;
+		name = i_name;
+		sub_id = i_sub_id;
+	};
+	int getId() { return id; }
+};
+
+class Professor : public User {
+	string oh;
+public:
+	Professor(int& id, string& name, list<string>& sub_id)
+		: User(id, name, sub_id) {}
+	void assign();
+	// getmaxnum_period, getmaxnum_day 이용해서 주의창 띄우기
+	// subject의 assign()을 이용
+	void edit_oh();
 };
 
 class Student : public User {
-	list<string> id_subjects; //과목 고유 코드를 갖고 있음
-
 public:
-	void check_sujects();
-	void check_assignment(); //subject 중에는 assignment가 존재하지 않을 수도 있음을 고려해야 함.
+	Student(int& id, string& name, list<string>& sub_id)
+		: User(id, name, sub_id) {}
+	void check_sujects() const;
+	void check_assignment() const; //subject 중에는 assignment가 존재하지 않을 수도 있음을 고려해야 함.
 	void check_oh(); //subject의 professor의 oh
 	void remove_subject(); //과목이 지워질때 id_subjects를 중 지워진 과목 코드를 삭제하는 함수
 };
 
-class Professor : public User {
-	string id_subject; //과목 고유 코드
-	string oh;
-public:
-	void assign();
-	// getmaxnum_period, getmaxnum_day 이용해서 주의창 띄우기
-	// subject의 assign()을 이용해서 s_assignments 수정
-	void edit_oh();
-};
 
 class Subject {
-	string id; //과목 고유 코드
-	string s_name;
+	string sub_id; //과목 고유 코드
+	string sub_name;
 	list<Assignment*> assignments; //더 이상 assignment는 subject를 상속받지 않음.
 public:
 	void assign(string id); //텍스트(2) //과제 객체를 만들어 admin에 파라미터로 주면 거기서 처리해줌
